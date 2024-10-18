8e513ce75b725aba11d9410d54dfa71a93b082bc
Sergeev O. V <o.v.sergeyev@gmail.com>
2024-10-18 18:21:20 +0300

    update

--- device-managment/migrations/env.py
+++ device-managment/migrations/env.py
@@ -11,6 +11,8 @@ from entities import UserEntity
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+from config import settings
+config.set_main_option("sqlalchemy.url", f"{settings.DATABASE_URL}?async_fallback=True")
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
