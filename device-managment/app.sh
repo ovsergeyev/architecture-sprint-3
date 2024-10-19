4c2ac631497ccbebfbe38e78c9ad9d60c60c0308
Sergeev O. V <o.v.sergeyev@gmail.com>
2024-10-19 08:29:45 +0300

    update

--- device-managment/app.sh
+++ device-managment/app.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-alembic revision --autogenerate -m "Initial migration"
+# alembic revision --autogenerate -m "Initial migration"
 alembic upgrade head
 
 # gunicorn main:app --workers 4 --worker-class uvicorn.workers.UvicornWorker --bind=0.0.0.0:8000
