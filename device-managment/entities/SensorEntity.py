7229c27733b3b9008ed05a524892767dd9c730ab
Sergeev O. V <o.v.sergeyev@gmail.com>
2024-10-19 10:15:46 +0300

    update

--- device-managment/entities/SensorEntity.py
+++ device-managment/entities/SensorEntity.py
@@ -1,8 +1,10 @@
 from database import Base
+from sqlalchemy import ForeignKey
 from sqlalchemy.orm import mapped_column, Mapped
+from schemas.SDevice import SDevice
 
 class SensorEntity(Base):
   __tablename__ = 'sensors'
 
   serial_number: Mapped[str] = mapped_column(primary_key=True)
-  device_id: Mapped[str]
+  device_id: Mapped[str] = mapped_column(ForeignKey('devices.serial_number'))
